e71ca626751e2aabd8eb095e2937aa188716f687
Anselmo Pfeifer <anselmo.pfeifer@tecsinapse.com.br>
2016-11-16 22:38:44 -0300

    Test de build usando o Android Studio

---
+++ modulo-03-build-android/Importing-gradle-projects/src/main/java/com/udacity/gradle/JokeTeller.java
@@ -0,0 +1,12 @@
+package com.udacity.gradle;
+
+/**
+ * Created by anselmo on 16/11/16.
+ */
+
+public class JokeTeller {
+
+    public static void main(String[] args){
+        System.out.println("Java. Write once, debug everywhere.");
+    }
+}
